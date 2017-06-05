5410c9ccb2e1feeb44a9e630a07e17474585fb62
grek-andrian <gretsko.andrian@gmail.com>
2017-06-06 00:51:21 +0300

    Add first test

--- src/students-list/student-component/student-component.test.js
+++ src/students-list/student-component/student-component.test.js
@@ -1,19 +1,113 @@
 import { assert } from 'chai';
 
-//import StudentComponent from './student-component';
+import StudentController  from './student-controller.js';
 
-let component;
+//let component;
 
-describe('StudentComponent', function() {
+describe('StudentController', function() {
+    //beforeEach(module('main.app.students-list'));
 
-    // beforeEach(function() {
-    //     component = new StudentComponent();
-    // });
+    // let $controller;
+    //
+    // beforeEach(inject(function(_$controller_){
+    //   $controller = _$controller_;
+    // }));
 
-    it('should return the correct result', () => {
 
-        assert.equal(1 + 1, 2);
+    //  beforeEach(function() {
+    //      controller = new StudentController();
+    //  });
+
+    let firebaseRef, controller, $rootscope;
+
+      beforeEach(() => {
+        window.MockFirebase.override();
+      });
+
+      beforeEach(() => {
+        window.MockFirebase.restore();
+      });
+
+      beforeEach(module('firebase.database', (_$firebaseRefProvider_) => {
+        _$firebaseRefProvider_.registerUrl('Mock');
+      }));
+
+      beforeEach(() => {
+        module('main.app.students-list');
+
+        inject(($controller, $firebaseArray, $firebaseRef, _$rootScope_) => {
+          $rootscope = _$rootScope_;
+
+          controller = $controller('StudentController', {
+            $firebaseRef: $firebaseRef,
+            $firebaseArray: $firebaseArray
+          });
+        });
     });
 
+    describe('Object Data Testing', () => {
 
+    beforeEach(() => {
+      inject(($firebaseRef) => {
+        firebaseRef = $firebaseRef.default.child('students');
+      });
+    });
+
+    it('should read object data from firebase', () => {
+      const student = {
+        firstname: 'John',
+        lastname: 'Doe',
+        email: 'jhon@ukr.net'
+      };
+
+      firebaseRef.set(student);
+      firebaseRef.flush();
+      $rootscope.$digest();
+
+      expect(controller.student.firstname).toEqual(student.firstname);
+    });
+
+    it('should write object data to firebase', () => {
+      let student;
+
+      // save some data that our controller will read
+      firebaseRef.on('value', (data) => {
+        student = data.val();
+      });
+
+      controller.updateStudent('Joe');
+      firebaseRef.flush();
+      $rootscope.$digest();
+
+      const keys = Object.keys(student);
+      expect(keys.length).toEqual(1);
+      expect(controller.student.name).toEqual(student.name);
+    });
+});
+
+    // it('should return the correct result', () => {
+    //     assert.equal(1 + 1, 2);
+    // });
+
+});
+
+describe('My controller', function () {
+  var scope, createController;
+
+  beforeEach(module('app.controllers'));
+
+  beforeEach(inject(function ($injector) {
+    var $rootScope = $injector.get('$rootScope');
+    var $controller = $injector.get('$controller');
+
+    scope = $rootScope.$new();
+
+    createController = function () {
+      return $controller('CustomCtrl', {$scope: scope});
+    };
+  }));
+
+  afterEach(function () {
+    scope.$destroy();
+  });
 });
